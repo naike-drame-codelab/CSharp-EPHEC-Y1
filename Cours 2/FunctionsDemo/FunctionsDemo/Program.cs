f7ab3f0f27809e062f18e90d3575d2e1f5a4c09f
Naïké Dramé <naike.drame.dev@gmail.com>
2026-02-05 19:50:07 +0100

    cours 2 - fonctions

---
+++ Cours 2/FunctionsDemo/FunctionsDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿// fonctions
+    // servent à structurer le programme
+    // permettent de réutiliser du code
+    // permettent d'éviter les répétitions
+Console.WriteLine();
+
+/* ---- 1) définition d'une fonction ---
+    // static : mot-clé indiquant que la fonction appartient à la classe et non à une instance de celle-ci
+    // returnType : type de valeur renvoyée par la fonction (void signifie que la fonction ne renvoie rien)
+    // NomDeLaFonction : nom de la fonction (Doit commencer par une majuscule par convention)
+    // (paramètres) : liste des paramètres d'entrée de la fonction
+*/
+static void Hello()
+{
+    Console.WriteLine("Bonjour !");
+}
+
+static void Bye()
+{
+    Console.WriteLine("Au revoir !");
+}
+
+// --- 2) appeler une fonction à l'endroit où l'on veut qu'elle soit exécutée ---
+Hello();
+Bye();
+Hello();
+Bye();
+Hello();
+Bye();
+
+// --- 3) fonction avec paramètres ---
+string userName = "Alice";
+Greet(userName);
+static void Greet(string name)
+{
+    Console.WriteLine($"Bonjour, {name} !");
+}
---
+++ Cours 2/IntroFonction_Net6/IntroFonction_Net6/Program.cs
@@ -0,0 +1,59 @@
+﻿// Série 1: fonctions sans paramètres
+// ==================================
+
+Console.WriteLine("Voici un triangle");
+DessinerTriangle();
+
+Console.WriteLine();
+Console.WriteLine("Voici un sapin");
+DessinerTriangle();
+DessinerTriangle();
+DessinerTriangle();
+DessinerTriangle();
+
+Console.WriteLine();
+
+// Série 2: fonctions avec paramètres
+// ==================================
+
+AfficherBonDAchat("Potter", "Harry", 50);
+AfficherBonDAchat("Granger", "Hermione", 100);
+
+string nomDuDirecteur = "Dumbledore";
+string prenomDuDirecteur = "Albus";
+int montantCadeauDuDirecteur = 1000;
+AfficherBonDAchat(nomDuDirecteur, prenomDuDirecteur, montantCadeauDuDirecteur);
+
+// Série 3: fonctions avec renvoi de valeur
+// =================================
+int montantCadeauRon;
+montantCadeauRon = CalculerMoyenne(50, 100);
+AfficherBonDAchat("Weasley", "Ron", montantCadeauRon);
+
+Console.WriteLine("Appuyez sur <enter> pour terminer...");
+Console.ReadLine();
+
+// Définitions des fonctions
+// =========================
+static void DessinerTriangle()
+{
+    Console.WriteLine("  *");
+    Console.WriteLine(" ***");
+    Console.WriteLine("*****");
+}
+
+static void AfficherBonDAchat(string nom, string prenom, int valeur)
+// Affiche un bon d'achat établi aux nom et prénom de la personne, pour la valeur donnée
+{
+    Console.WriteLine("==========================================");
+    Console.WriteLine("Bon d'achat d'une valeur de " + valeur + " euros");
+    Console.WriteLine("Etabli au nom de " + prenom + " " + nom);
+    Console.WriteLine("==========================================");
+}
+static int CalculerMoyenne(int nombre1, int nombre2)
+// Renvoie la moyenne des deux nombres, arrondie à l'entier inférieur
+{
+    int resultat;
+    resultat = (nombre1 + nombre2) / 2;
+    return resultat;
+}
