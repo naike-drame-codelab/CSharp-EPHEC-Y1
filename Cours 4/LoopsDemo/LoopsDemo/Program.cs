0370288d9078e419353b95aabd5b57672489278e
Naïké Dramé <naike.drame.dev@gmail.com>
2026-02-19 20:52:13 +0100

    exos conditions 2

---
+++ Cours 4/LoopsDemo/LoopsDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿// while - compteur par 2
+CompteurPar2();
+Console.WriteLine();
+
+// while - compteur par 7
+CompteurPar7();
+Console.WriteLine();
+
+// while - nombres impairs
+NombresImpairs();
+Console.WriteLine();
+
+// while - carré des entiers
+Console.Write("Entrez le nombre d'entiers à calculer : ");
+int x = int.Parse(Console.ReadLine());
+CarreEntier(x);
+
+static void CompteurPar2()
+{
+    int i = 0;
+    while ((i <= 20) && (i*2 <= 20))
+    {
+        Console.WriteLine(i*2);
+        i++;
+    }
+}
+
+static void CompteurPar7()
+{
+    int i = 0;
+    while (i <= 20)
+    {
+        Console.WriteLine(i);
+        i += 2;
+    }
+}
+
+static void NombresImpairs()
+{
+    int i = 0;
+    while (i <= 20)
+    {
+        if(i % 2 != 0)
+        {
+            Console.WriteLine(i);
+        }
+        i++;
+    }
+}
+
+static void CarreEntier(int x)
+{
+    int i = 1;
+    while (i <= x) { 
+        Console.WriteLine(i * i);
+        i++;
+    }
+}
